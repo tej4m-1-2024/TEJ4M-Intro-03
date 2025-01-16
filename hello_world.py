c5740bd6bcc078d31457c5d45723677707740df3
TEJ4M-1-2024 <tej4m-1-2024@mths.ca>
2025-01-16 10:11:41 -0500

    linter test

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,6 @@ Created by: Mr. Coxall
 Created on: Sept 2020
 This is the "Hello, World!" module, with proper style
 """
-
-
 def main() -> None:
     """The main() function prints out Hello, World!, returns None."""
     print("Hello, World!")
