432f651142c8f1afedc66c4ef334b5713790f04a
TEJ4M-1 2024 <tej4m-1-2024@mths.ca>
2025-01-16 09:02:08 -0500

    working version

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Created by: Mr. Coxall
+Created on: Sept 2020
+This is the "Hello, World!" module, with proper style
+"""
+
+
+def main() -> None:
+    """The main() function prints out Hello, World!, returns None."""
+    print("Hello, World!")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
