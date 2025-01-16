f8428eb3eb89df26ab54200653f1acf60531b020
TEJ4M-1-2024 <tej4m-1-2024@mths.ca>
2025-01-16 10:15:13 -0500

    linter does work

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,8 @@ Created by: Mr. Coxall
 Created on: Sept 2020
 This is the "Hello, World!" module, with proper style
 """
+
+
 def main() -> None:
     """The main() function prints out Hello, World!, returns None."""
     print("Hello, World!")
